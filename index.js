c510807ac4c5e98557b101892c2961c0b3c020cc
mbiederman <megna.biederman@vonage.com>
2022-06-07 21:11:31 -0700

    added list of archives

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ app.use(express.urlencoded({
 }))
 
 // Create Session + Store in Express App
-opentok.createSession(function (err, session) {
-    if (err) return console.log(err);
-    app.set('sessionId', session.sessionId);
+opentok.createSession({mediaMode:"routed"},function (err, session) {
+  if (err) return console.log(err);
+  app.set('sessionId', session.sessionId);
   
-    app.listen(3000, function () {
-        console.log('Server listening on PORT 3000');
-    });
+  app.listen(3000, function () {
+    console.log('Server listening on PORT 3000');
+  });
 });
 
 app.get('/', function (req, res) {
@@ -44,11 +44,11 @@ app.get('/', function (req, res) {
 
 
 app.get('/newsession', (req, res)=>{
-    var sessionId = app.get('sessionId');
+  var sessionId = app.get('sessionId');
 
   // Generate a Token from the sessionId
-    var token = opentok.generateToken(sessionId);
-    invSessionId = false;
+  var token = opentok.generateToken(sessionId);
+  invSessionId = false;
 
 
   // Renders Views (Sends HTML to client) + pass in variables: apiKey, sessionId, token
@@ -59,11 +59,7 @@ app.get('/newsession', (req, res)=>{
   });
 });
 
-app.get('/archives', (req, res)=>{
-    res.render('archives.ejs',{
 
-    })
-});
 
 app.post('/auth', function(req, res){
   //get sessionID from user
@@ -85,4 +81,87 @@ app.post('/auth', function(req, res){
 
   
 
+})
+
+app.post('/start', function(req, res){
+
+  var archiveName = req.body.archiveName;
+  
+  opentok.startArchive(app.get('sessionId'), { name: archiveName }, function (
+    err,
+    archive
+  ) {
+    if (err) {
+      return console.log(err);
+    } else {
+    
+      console.log("new archive:" + archive.id);
+      app.set('archiveId', archive.id);
+    }
+  });
+
+  res.redirect('/newsession')
+  
+})
+
+app.get('/stop', function(req, res){
+  opentok.stopArchive(app.get('archiveId'), function (err, archive) {
+    if (err) return console.log(err);
+  
+    console.log("Stopped archive:" + archive.id);
+  });
+  
+  res.redirect('/newsession')
+})
+
+app.get('/archives', (req, res)=>{
+
+  return res.render('archives.ejs',{
+  })
+})
+
+app.get('/listarchives', (req, res)=>{
+  opentok.listArchives( function (
+    error,
+    archives,
+    totalCount
+  ) {
+    if (error) return console.log("error:", error);
+  
+    console.log(totalCount + " archives");
+
+    for (var i = 0; i < archives.length; i++) {
+      
+      console.log(archives[i].id);
+      
+    }
+    return res.render('archiveHistory.ejs',{
+      archives: archives
+    })
+  });
+  
+
+
+});
+
+app.post('/getarchive', function(req, res){
+  var archiveId= req.body.archiveId;
+
+  opentok.getArchive(archiveId, function (err, archive) {
+    if (err) return res.send(500, 'Could not get archive ' + archiveId + '. error=' + err.message);
+    return res.redirect(archive.url);
+  });
+})
+
+app.post('/deletearchive', function(req, res){
+  var archiveId= req.body.deleteArchive;
+
+  opentok.deleteArchive(archiveId, function (err) {
+    if (err) return res.send(500, 'Could not get archive ' + archiveId + '. error=' + err.message);
+
+    return res.redirect("listarchives");
+  });
+ 
+
+  
 })
--- public/js/stream.js
+++ public/js/stream.js
@@ -1,42 +1,52 @@
+
+
+
 initializeSession();
+
 
 
 // Handling all of our errors here by alerting them
 function handleError(error) {
-    if (error) {
-      alert(error.message);
+  if (error) {
+    alert(error.message);
       
-    }
   }
+}
+
+
+
   
-  function initializeSession() {
-    var session = OT.initSession(apiKey, sessionId);
+function initializeSession() {
+  var session = OT.initSession(apiKey, sessionId);
     
-    // Subscribe to a newly created stream
+  // Subscribe to a newly created stream
   
-    session.on('streamCreated', function(event) {
-        session.subscribe(event.stream, 'subscriber', {
+  session.on('streamCreated', function(event) {
+    session.subscribe(event.stream, 'subscriber', {
           insertMode: 'append',
           width: '100%',
           height: '100%'
-        }, handleError);
-      });
+    }, handleError);
+  });
          
 
-    // Create a publisher
-    var publisher = OT.initPublisher('publisher', {
+  // Create a publisher
+  var publisher = OT.initPublisher('publisher', {
       insertMode: 'append',
       width: '100%',
       height: '100%'
-    }, handleError);
+  }, handleError);
   
-    // Connect to the session
-    session.connect(token, function(error) {
-      // If the connection is successful, publish to the session
-      if (error) {
-        handleError(error);
-      } else {
-        session.publish(publisher, handleError);
-      }
-    });
-  }
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
+
+  
+}
+
